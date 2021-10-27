301ddd73c41e21a7dd25f60e304f32b51ae0cb01
Almendra Salgado Gatica <almendra@dhcp-v100-021.mobile.uci.edu>
2021-10-27 09:02:57 -0700

    adding reward for box on

--- state.py
+++ state.py
@@ -29,8 +29,22 @@ class State(object):
         return board    
     
     def checkBoard(self, data, action):
+        print("# boxes: %s - Location of boxes:%s" % (self.numBoxes, self.boxes))
+        print("# storages: %s - Location of storages:%s" % (self.numStorage, self.storage))
+
+        e_newPosition = (data.agent.row + action[0], data.agent.col + action[1])
+        e_boxPosition = (e_newPosition[0] + action[0], e_newPosition[1] + action[1])
+        print("Expected new position of the box: (%s, %s)" % (e_boxPosition[0], e_boxPosition[1]))
+
+        if e_boxPosition in self.storage:
+            ## Check if it is a valid position for storage
+            if e_boxPosition not in self.boxes:
+                return "box on"
+            ## If it is already in the boxes array, the location should be used by another box.
+
         if self.isGameOver(data):
             return "win"
+
 
     def isGameOver(self, data): 
         for (row, col) in self.boxes:
@@ -49,4 +63,7 @@ class State(object):
         if not isinstance(other, State): 
             return False
         return str(self.board) == str(other.board)
+
+    #def __repr__(self):
+    #    return "<Agent row:%s col:%s q:%s>" % (self.row, self.col, self.q)
 
